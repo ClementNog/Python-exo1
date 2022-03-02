7a7b76e5f84e18d9ad357d895748ae11047565c9
NOGUEIRE Clement <nogucle@ndlp.fr>
2022-03-02 15:37:19 +0100

    Add method (nomdossier ex: User.check-password())

--- user.py
+++ user.py
@@ -1,5 +1,8 @@
 class User:
     pass
+    def check_password(self, password):
+        
+        return self.password == password
 
 john = User()
 
@@ -9,3 +12,7 @@ john.password = '12345'
 print((f'Bonjour, je suis {john.name}.'))
 print((f'Mon id est {john.id}.'))
 print((f'Mon mot de passe est {john.password}.'))
+testpassword1= input('entrez un mot de passe')
+testpassword2= input('entrez un autre mot de passe')
+print(f'Verification du mot de passe "1234" = {john.check_password(testpassword1)}')
+print(f'Vérification du mot de passe "12345" = {john.check_password(testpassword2)}')
