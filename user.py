428cf3bdd72d6ac72b4360eaeb02653351972a95
NOGUEIRE Clement <nogucle@ndlp.fr>
2022-03-02 15:23:37 +0100

    Add method User.check-password()

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    pass
+
+john = User()
+
+john.id = 1
+john.name = 'John'
+john.password = '12345'
+print((f'Bonjour, je suis {john.name}.'))
+print((f'Mon id est {john.id}.'))
+print((f'Mon mot de passe est {john.password}.'))
