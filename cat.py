e3ea3e8922e5d4e3fb4fb0e6ea827a6e510db210
NOGUEIRE Clement <nogucle@ndlp.fr>
2022-03-09 17:03:07 +0100

    Add method cat.py

--- cat.py
+++ cat.py
@@ -1,11 +1,17 @@
+'''from pickle import _BufferCallback
 class Cat:
-    pass
+    def __init__(self, name, size, color):
+        self.name = name
+        self.size = size
+        
     def grow(self):
         nbrtaille = int(input('de quelle taille a grandi le chat '+ self.name + ' '))
         self.size += nbrtaille
         return self.name, self.size
     def showcolor(self):
         return self.name, self.color
+class Dog:
+    pass
 
 
 chat1 = Cat()
@@ -19,4 +25,26 @@ chat2.color = 'black'
 print(f"Voici les anciennes tailles de {chat1.name , chat1.size} et de {chat2.name , chat2.size} et maintenant voici leur taillle actuel {chat1.grow()} et {chat2.grow()}")
 if chat2.size > chat1.size:
     print('cheh !')
-print(f"{chat1.showcolor() , chat2.showcolor()}")
+print(f"{chat1.showcolor() , chat2.showcolor()}")'''
+
+class Animal:
+    def be_happy(self):
+        raise Exception('not implemented')
+class Dog(Animal):
+    def be_happy(self):
+        print("ouaf,ouaf")
+
+class Cat(Animal):
+    def be_happy(self):
+        print('miaou, miaou')
+
+class Canary(Animal):
+    def be_happy(self):
+        print("coin, coin")
+animal1 = Dog()
+animal2 = Cat()
+animal3 = Canary()
+
+animal1.be_happy()
+animal2.be_happy()
+animal3.be_happy()
