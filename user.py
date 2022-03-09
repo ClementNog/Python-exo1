3d720c4a06001b3bc23b17e4c310aac3c0921a62
NOGUEIRE Clement <nogucle@ndlp.fr>
2022-03-09 16:43:29 +0100

    Add method user.py

--- user.py
+++ user.py
@@ -7,6 +7,8 @@ class User:
         
     def check_password(self, password):
         return self.password == password
+    def __str__(self):
+        return f"{self.name , self.id}"
     
 
 class Admin(User):
@@ -19,27 +21,25 @@ class Superadmin(Admin):
         print("je suis un super administrateur")
         return self
     
-john = User()
-jean = Admin()
-bob = Superadmin()
-john.id = 1
-john.name = 'John'
-john.password = '12345'
-jean.id = 2
-jean.name = 'Jean'
-jean.password = '1234'
-bob.id = 3
-bob.name = 'Bob'
-bob.password = '123'
+user1 = User(1,'John','12345')
+user2 = Admin(2,'John', '1234')
+user3 = Superadmin(3, 'Bob', '123')
 
-print((f'Bonjour, je suis {john.name}.'))
-print((f'Mon id est {john.id}.'))
-print((f'Mon mot de passe est {john.password}.'))
+
+'''print((f'Bonjour, je suis {user2.name}.'))
+print((f'Mon id est {user2.id}.'))
+print((f'Mon mot de passe est {user2.password}.'))
 testpassword1= input('entrez un mot de passe')
 testpassword2= input('entrez un autre mot de passe')
-print(f'Verification du mot de passe "1234" = {john.check_password(testpassword1)}')
-print(f'Vérification du mot de passe "12345" = {john.check_password(testpassword2)}')
+print(f'Verification du mot de passe "1234" = {user2.check_password(testpassword1)}')
+print(f'Vérification du mot de passe "12345" = {user2.check_password(testpassword2)}')
 
 
-print(f"Verif jean = {jean.manage()}")
-print(f"Verif bob = {bob.manage()}")
+print(f"Verif user1 = {user1.manage()}")
+print(f"Verif user3 = {user3.manage()}")'''
+etablissement = 'Lycée NDLP'
+service = 'Service Informatique'
+liste_divers= [etablissement, service, user1, user2, user3]
+
+for item in liste_divers:
+    print(item)
